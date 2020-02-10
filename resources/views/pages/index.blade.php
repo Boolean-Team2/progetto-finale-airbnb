866073b001b48859cf0400a316f23a4bbf5e50b4
Marco-S117 <polino47@gmail.com>
2020-02-10 10:11:48 +0100

    First test

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,9 @@
 @extends('templates.template')
 
 @section('main')
-    
+    @foreach ($apartments as $apartment)
+        <div>
+            {{ $apartments }}
+        </div>        
+    @endforeach
 @endsection
