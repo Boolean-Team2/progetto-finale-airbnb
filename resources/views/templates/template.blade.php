6dd72295a9a02504c8f5fe99352a244051a3441d
Marco-S117 <polino47@gmail.com>
2020-02-10 13:10:06 +0100

    Edited greeting
    The time they were set shows the user's first and last name, otherwise their emai is shown.

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -51,10 +51,17 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    @if (Auth::user()->firstname || Auth::user()->lastname)
+                                        {{ Auth::user()->firstname }} {{ Auth::user()->lastname }} 
+                                        @else
+                                            {{ Auth::user()->email }}
+                                    @endif <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('account.show', Auth::user()->id) }}">
+                                        {{ __('My Profile') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
