6d22fad96c11d2087cc839f013a6aac9438232d5
Marco-S117 <polino47@gmail.com>
2020-02-10 19:39:31 +0100

    Added messages fadeout effect

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,3 +30,10 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
 });
+
+$(document).ready(init);
+
+function init() {
+    // MESSAGE OUT ANIMATION
+    $('div.alert').not('div.alert.alert-info').delay(3500).fadeOut(450);
+}
