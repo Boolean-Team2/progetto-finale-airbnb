db6e5c7aee4af645b1b88e9de10fefa3902ab272
DevAngelo98 <ainglima07@gmail.com>
2020-02-11 19:33:51 +0100

    Fixed image bug

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/assets/js')
-   .copy('resources/images/**/*.{jpg,jpeg,png,gif}', 'public/images', { base: 'resources/images' })
+   .copy('resources/images/**/*.{jpg,jpeg,png,gif}', 'public/assets/images', { base: 'resources/images' })
    .sass('resources/sass/app.scss', 'public/assets/css')
    .options({
       processCssUrls: false
