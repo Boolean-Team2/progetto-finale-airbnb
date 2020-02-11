f309ac13f72bc140d42b677fd91a5ea7955dc630
Marco-S117 <polino47@gmail.com>
2020-02-11 12:20:25 +0100

    Added link to user's apartments

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -39,6 +39,9 @@
                             <a class="dropdown-item" href="{{ route('account.show', Auth::user()->id) }}">
                                 {{ __('My Profile') }}
                             </a>
+                            <a class="dropdown-item" href="{{ route('account.apartments.show', Auth::user()->id) }}">
+                                {{ __('My Apartments') }}
+                            </a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                             onclick="event.preventDefault();
                                             document.getElementById('logout-form').submit();">
