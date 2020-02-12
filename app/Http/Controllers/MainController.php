1bf014599a14a4599e04d97f2567c27a8f8c4113
Marco-S117 <polino47@gmail.com>
2020-02-12 23:38:22 +0100

    Added single apartment show function

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,7 +10,15 @@ class MainController extends Controller
 {
     public function index() {
 
-        $apartments = Apartment::paginate(4);
+        // fare query per appartamenti sponsorizzati, aggiungere campo nel db$authorId = 956;
+
+        $apartments = Apartment::orderBy('id', 'DESC')->paginate(4);
         return view('pages.index', compact('apartments'));
+    }
+
+    // Apartment details show
+    public function apartmentShow($id) {
+        $apartment = Apartment::findOrFail($id);
+        return view('pages.apartments.apartmentShow', compact('apartment'));
     }
 }
