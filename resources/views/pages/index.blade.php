2f950acd21364f722084d1e6e304240a4ef5a28d
Marco-S117 <polino47@gmail.com>
2020-02-18 18:36:13 +0100

    Apartments now show in page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,7 +8,7 @@
     {{-- INCLUDE NAVBAR --}}
     @include('partials.navbar')
 
-    {{-- SEARCH VUE COMPONENT --}}
+    {{-- SEARCH --}}
     <div id="app" class="container-fluid h-100">
         <div class="row h-75 align-items-center">
             <div class="col-sm-12 col-md-10 col-lg-4 offset-md-1 bg-white p-3">
@@ -55,6 +55,29 @@
     </div>
 </header>
 
+{{-- CDN HANDLEBARS --}}
+<script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
+{{-- TEMPLATE HANDLEBARS --}}
+<script id="hbApTemplate" type="text/x-handlebars-template">
+    <div class="card mb-3" style="width: 20rem;">
+        <img class="img-fluid p-1" src="http://localhost:3000/assets/images/users/@{{user_id}}/apartments/@{{id}}/@{{img}}"  alt="Card image cap">
+        <div class="card-body">
+            <p class="d-flex justify-content-between">
+                <a class="text-primary" href="@{{showUrl}}">@{{name}}</a>
+                <span><i class="mr-1 fas fa-eye"></i>@{{views}}</span>
+            </p>
+            <p class="d-flex justify-content-between">
+                <span><i class="mr-1 fas fa-bed"></i>@{{beds}}</span>
+                <span><i class="mr-1 fas fa-users"></i>@{{rooms}}</span>
+                <span><i class="mr-1 fas fa-toilet-paper"></i>@{{baths}}</span>
+                <span><i class="mr-1 fas fa-ruler-combined"></i>@{{mq}}</span>
+            </p>
+            <p>Distanza dalla ricerca: @{{km}} km</p>
+        </div>
+    </div>
+</script>
+
+{{-- AJAX CALL SCRIPT --}}
 <script>
     $(document).ready(function() {
 
@@ -81,7 +104,7 @@
         // Funzione di ricerca appartamenti
         function search(input, radius, beds, rooms, services){
             console.clear();
-            console.log("Ricerca:", input, "Raggio", radius, "Letti", beds, "Stanze", rooms, "Services:", services);
+            // console.log("Ricerca:", input, "Raggio", radius, "Letti", beds, "Stanze", rooms, "Services:", services);
             $.ajax({
                 url: "https://api.tomtom.com/search/2/geocode/"+ input +".json",
                 method: 'GET',
@@ -90,11 +113,11 @@
                     key: 'UnotVndyZgjPLoXejGGoIUZDc49X2IrU'
                 },
                 success: function (data) {
-                        // console.log(data);
-                        var results = data.results;
-                        var lat = results[0].position.lat;                   
-                        var lon = results[0].position.lon;
-                        // Chiamata al nostro db che restituisce tutti gli appartamenti
+                    // console.log(data);
+                    var results = data.results;
+                    var lat = results[0].position.lat;                   
+                    var lon = results[0].position.lon;
+                    // Chiamata al nostro db che restituisce tutti gli appartamenti
                     $.ajax({
                         url: "http://localhost:3000/api/apartments/show",
                         method: 'GET',
@@ -107,12 +130,38 @@
                             services : services
                         },
                         success: function (data) {
-                            console.log("Dati del db:",data);
+                            // console.log("Dati del db:",data);
+                            printData(data);
                         },
                         error: function (error) {
                             console.log("Si è verificato un errore", error);
                         }
                     });
+                    // PrintData function
+                    function printData(res) {
+                        console.log("Devo stampare in pagina:", res);
+
+                        var output = $("#js_hbOutput").html("");
+                        var sorgente = $("#hbApTemplate").html();
+                        var template = Handlebars.compile(sorgente);
+                        var temp;
+
+                        res.apps.forEach(oggetto => {
+                            temp = oggetto;
+
+                            var dist = oggetto.km.toFixed(2);
+                            temp['km'] = dist;
+
+                            var id = oggetto.id;
+                            var routeUrl = '{{ route("apartment.show", ":id") }}';
+                            routeUrl = routeUrl.replace(':id', id);
+                            temp['showUrl'] = routeUrl;
+
+                            html = template(temp);
+                            output.append(html);
+                        });
+                    }
+                    // Fine Chiamata al nostro db che restituisce tutti gli appartamenti
                 },
                 error: function () {
                     console.log("Si è verificato un errore");
@@ -124,11 +173,11 @@
 
 <main>    
     <div class="container">
-        {{-- PREMIUM APARTMENTS --}}
         <section class="my-5">
             <div class="row">
-                <div class="col-sm-12 p-3">
-                    <h3>Alloggi Sponsorizzati</h3>
+                {{-- HANDLEBARS OUTPUT --}}
+                <div id="js_hbOutput" class="col-sm-12 d-flex flex-wrap justify-content-between">
+                    {{-- <h3>Alloggi Sponsorizzati</h3>
                     <p>Una selezione di alloggi verificati per qualità e design.</p>
                     <div class="d-flex flex-wrap justify-content-between">
                         @foreach ($apartments as $apartment)
@@ -146,38 +195,10 @@
                                 <p>Visibility: {{ $apartment->visibility }}</p>
                             </div>        
                         @endforeach
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </section>
-        {{-- /PREMIUM APARTMENTS --}}
-        {{-- FREE APARTMENTS --}}
-        <section class="my-5">
-            <div class="row">
-                <div class="col-sm-12">
-                    <h3>Alloggi non sponsorizzati</h3>
-                    <p>Una selezione di alloggi verificati per qualità e design.</p>
-                    <div class="d-flex flex-wrap justify-content-between">
-                        @foreach ($apartments as $apartment)
-                            <div class="card mb-3" style="width: 20rem;">
-                                @if ($apartment->img)
-                                    <img class="img-fluid" src="{{ asset('assets/images/users/' . $apartment->user_id . "/apartments/" . $apartment->id . "/" . $apartment->img) }}" alt="Card image cap">
-                                    @else
-                                    <img class="img-fluid" src="{{ asset('assets/images/placeholder.jpg') }}" alt="Card image cap">
-                                @endif
-                                <div class="card-body">
-                                    <h5 class="card-title text-capitalize">{{ $apartment->name }}</h5>
-                                    <p class="card-text text-capitalize">{{ $apartment->description }}</p>
-                                    <a href="{{ route('apartment.show', $apartment->id) }}" class="btn btn-primary">Show details</a>
-                                </div>
-                                <p>Visibility: {{ $apartment->visibility }}</p>
-                            </div>        
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        </section>
-        {{-- /FREE APARTMENTS --}}
     </div>
 </main>
 
