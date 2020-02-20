49c733a274b9609e54be8078d301cbf679122de9
Marco-S117 <polino47@gmail.com>
2020-02-20 13:01:18 +0100

    Edited html

--- resources/views/pages/users/apartments/statistics.blade.php
+++ resources/views/pages/users/apartments/statistics.blade.php
@@ -8,47 +8,47 @@
 {{-- CONTENT --}}
 @section('body')
 <div class="container my-5 py-5">
-  <div class="row">
-      <div class="col-12">
-          @if (session('status'))
-              <div class="alert alert-success">
-                  {{ session('status') }}
-              </div>
-          @endif
-          @if ($errors->any())
-              <div class="alert alert-danger">
-                  <ul class="m-0">
-                      @foreach ($errors->all() as $error)
-                          <li>{{ $error }}</li>
-                      @endforeach
-                  </ul>
-              </div>
-          @endif
-      </div>
-  </div>
+    <div class="row">
+        <div class="col-12">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul class="m-0">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+    </div>
 </div>
 <div class="container">
-  <div class="row">
-    <div class="col-sm-12 col-lg-6">
-      <h3> Views number</h3>
-      <p id="views">{{$apartment->views}}</p>
-      <div class="container">
-        <div class="wrap">
-          <canvas id="myViews"></canvas> 
+    <div class="row">
+        <div class="col-sm-12 col-lg-6">
+        <h3> Views number</h3>
+        <p id="views">{{$apartment->views}}</p>
+        <div class="container">
+            <div class="wrap">
+            <canvas id="myViews"></canvas> 
+            </div>
         </div>
-      </div>
-    </div>
-    
-    <div class="col-sm-12 col-lg-6">
-      <h3> Messages number</h3>
-      <p id="messages">{{ $apartment->messages->count() }}</p>
-      <div class="container">
-        <div class="wrap">
-          <canvas id="myMessages"></canvas> 
         </div>
-      </div>
+        
+        <div class="col-sm-12 col-lg-6">
+        <h3> Messages number</h3>
+        <p id="messages">{{ $apartment->messages->count() }}</p>
+        <div class="container">
+            <div class="wrap">
+            <canvas id="myMessages"></canvas> 
+            </div>
+        </div>
+        </div>
     </div>
-  </div>
 </div>
 
 {{-- FOOTER --}}
@@ -56,9 +56,8 @@
 
 {{-- 
     IDEE GRAFICI 
-    - TORTA CON VIEWS DEI SINGOLI APPARTAMENTI    
+    - DIVIDERE LE VISUALS PER DATA    
 --}}
-
 
 <script>
   
