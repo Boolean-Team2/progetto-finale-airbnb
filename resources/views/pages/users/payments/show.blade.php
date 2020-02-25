d7b65039c53c8ec84b0ec6dae16058dc522cacbb
Sofiag19 <sofia.giacometti.19@gmail.com>
2020-02-25 16:06:54 +0100

    Payments similat to messages

--- resources/views/pages/users/payments/show.blade.php
+++ resources/views/pages/users/payments/show.blade.php
@@ -35,25 +35,44 @@
         <div class="row">
             <div class="col-sm-12">
                 <h3 class="mb-3">Your payments</h3>
-                @foreach ($user->apartments as $apartment)
-                    
-                    @foreach ($apartment->ads as $ad)
-
-                        @if (($apartment->ads->count()) > 0)
-
-                            Nome appartamento: {{ $apartment->name }} <br>
-                            Sponsorizzazione: {{ $ad->name }} <br>
-                            Periodo: da {{ $ad->pivot->start_time }} a {{ $ad->pivot->end_time }} <br>
-                            Prezzo: {{ $ad->price }} € <br><br>
-                            
-                        @endif
-                    
-
+                <div class="row">
+                    <div class="d-none d-sm-block col-md-3">
+                        <h6 class="text-uppercase font-weight-bold">Apartment Name</h6>
+                    </div>
+                    <div class="d-none d-sm-block col-md-2">
+                        <h6 class="text-uppercase font-weight-bold">Ad Type</h6>
+                    </div>
+                    <div class="d-none d-sm-block col-md-2">
+                        <h6 class="text-uppercase font-weight-bold">Ad Price</h6>
+                    </div>
+                    <div class="d-none d-sm-block col-md-5">
+                        <h6 class="text-uppercase font-weight-bold">Ad Period</h6>
+                    </div>
+                    @foreach ($user->apartments as $apartment)
+                        @foreach ($apartment->ads as $ad)
+                            @if (($apartment->ads->count()) > 0)
+                            <div class="col-sm-12 col-md-3">
+                                {{ $apartment->name }}
+                            </div>
+                            <div class="col-sm-12 col-md-2">
+                                {{ $ad->name }}
+                            </div>
+                            <div class="col-sm-12 col-md-2">
+                                {{ $ad->price }}€
+                            </div>
+                            <div class="col-sm-12 col-md-5">
+                                from {{ $ad->pivot->start_time }} to {{ $ad->pivot->end_time }}
+                            </div>
+                            @endif
+                            @endforeach
                     @endforeach
 
-                @endforeach
+                </div>
             </div>
         </div>
+        <hr>
+        <div class="text-right"> Total payments: {{ $apartment->ads->sum('price') }}€ </div>
+        <hr>
     </div>
 
     {{-- INCLUDE FOOTER --}}
