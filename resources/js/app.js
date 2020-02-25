84d0df949b981e90f90316e721ccb1b55f0c3add
Marco-S117 <polino47@gmail.com>
2020-02-26 00:02:50 +0100

    Rimossa animazione navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,14 +13,6 @@ function init() {
     // MESSAGE OUT ANIMATION
     $('div.alert').not('div.alert.alert-info').delay(3500).fadeOut(450);
 
-    // NAVBAR ANIMATION
-    $(window).bind('scroll', function () {
-        if ($(window).scrollTop() > 90) {
-            $('.navbar').addClass('fixed-top bg-primary shadow');
-        } else {
-            $('.navbar').removeClass('fixed-top bg-primary shadow');
-        }
-    });
 }
 
 
