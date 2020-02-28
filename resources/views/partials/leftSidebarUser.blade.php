468f567fd5f6f0102dea6eebf58ebd26213745ab
Marco-S117 <polino47@gmail.com>
2020-02-28 22:05:16 +0100

    Cambiato l'aspetto del menu utente

--- resources/views/partials/leftSidebarUser.blade.php
+++ resources/views/partials/leftSidebarUser.blade.php
@@ -1,21 +1,21 @@
-<h3>Link utili</h3>
-<ul class="list-unstyled list-group">
+<h3>Account menu</h3>
+<ul class="list-unstyled list-group-item-action">
     <li>
-        <a class="list-group-item text-primary" href="{{ route('account.show', Auth::user()->id) }}">{{ __('Profile') }}</a>
+        <a class="list-group-item list-group-item-action" href="{{ route('account.show', Auth::user()->id) }}">{{ __('Profile') }}</a>
     </li>
     <li>
-        <a class="list-group-item text-primary" href="{{ route('account.apartments.show', Auth::user()->id) }}">{{ __('Apartments') }}</a>
+        <a class="list-group-item list-group-item-action" href="{{ route('account.apartments.show', Auth::user()->id) }}">{{ __('Apartments') }}</a>
     </li>
     <li>
-        <a class="list-group-item text-primary" href="{{ route('account.statistics.show', Auth::user()->id) }}">{{ __('Statistics') }}</a>
+        <a class="list-group-item list-group-item-action" href="{{ route('account.statistics.show', Auth::user()->id) }}">{{ __('Statistics') }}</a>
     </li>
     <li>
-        <a class="list-group-item text-primary" href="{{ route('user.sponsor.payments', Auth::user()->id) }}">{{ __('Payments') }}</a>
+        <a class="list-group-item list-group-item-action" href="{{ route('user.sponsor.payments', Auth::user()->id) }}">{{ __('Payments') }}</a>
     </li>
     <li>
-        <a class="list-group-item text-primary" href="{{ route('account.messages.show', Auth::user()->id) }}">{{ __('Emails') }}</a>
+        <a class="list-group-item list-group-item-action" href="{{ route('account.messages.show', Auth::user()->id) }}">{{ __('Emails') }}</a>
     </li>
     <li>
-        <a class="list-group-item text-primary" href="{{ route('chats') }}">{{ __('Chat messages') }}</a>
+        <a class="list-group-item list-group-item-action" href="{{ route('chats') }}">{{ __('Chat messages') }}</a>
     </li>
 </ul>
