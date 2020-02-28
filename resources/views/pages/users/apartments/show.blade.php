4515889a8560fed292baec13483cb97906a30f16
Marco-S117 <polino47@gmail.com>
2020-02-28 22:05:43 +0100

    Aggiunta richiesta di conferma prima di eliminare l'appartamento

--- resources/views/pages/users/apartments/show.blade.php
+++ resources/views/pages/users/apartments/show.blade.php
@@ -41,7 +41,27 @@
                                         <a class="ml-2" href="{{ route('account.apartment.edit', $apartment->id) }}"><i class="far fa-edit fa-lg"></i></a>
                                         <a class="ml-2" href="{{ route('apartmet.statistics.show', [Auth::user()->id, $apartment->id]) }}"><i class="fas fa-chart-pie fa-lg"></i></a>
                                         <span style="cursor: pointer;" id="js_showEndTimeAd" class="ml-2 text-success"><i class="fas fa-ad fa-lg"></i></span>
-                                        <a class="ml-2 text-muted" href="{{ route('account.apartment.delete', [Auth::user()->id, $apartment->id]) }}"><i class="fas fa-trash-alt fa-lg"></i></a>
+                                        <a class="ml-2 text-muted" href="#" onclick="deleteData({{Auth::user()->id}})" data-toggle="modal" data-target="#DeleteModal"><i class="fas fa-trash-alt fa-lg"></i></a>
+                                        <!-- Modal content-->
+                                        <div style="z-index: 999999" id="DeleteModal" class="modal fade text-danger mt-5" role="dialog">
+                                            <div class="modal-dialog ">
+                                                <div class="modal-content">
+                                                    <div class="modal-header bg-danger justify-content-center align-items-center">
+                                                        <h4 class="modal-title text-white">DELETE CONFIRMATION</h4>
+                                                    </div>
+                                                    <div class="modal-body text-center">
+                                                        <span>Are sure you want to delete your this apartment?</span><br>
+                                                        <span>The action is <strong>irreversible</strong>!</span><br>
+                                                        <span>All sponsorship payments will not be refunded.</span><br>
+                                                        <span>All messages and email will no longer be available.</span><br>
+                                                    </div>
+                                                    <div class="modal-footer d-flex justify-content-between">
+                                                        <a href="#" class="btn btn-warning" data-dismiss="modal">Cancel</button>
+                                                        <a href="{{ route('account.apartment.delete', [Auth::user()->id, $apartment->id]) }}" class="btn btn-danger">Confirm</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                                 <p class="js_adEndTime card-text text-center mt-1">
