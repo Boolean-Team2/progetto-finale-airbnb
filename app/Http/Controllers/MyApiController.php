73b26050939a39fe7ee59d3443fa237ba1f09c5a
Marco-S117 <polino47@gmail.com>
2020-02-29 13:14:30 +0100

    Ora non viene ritornato lo stesso appartamento in quelli vicini

--- app/Http/Controllers/MyApiController.php
+++ app/Http/Controllers/MyApiController.php
@@ -109,18 +109,27 @@ class MyApiController extends Controller
             $lat2 = $apartament['latitude'];
             $lon2 = $apartament['longitude'];
 
-            $theta = $lon1 - $lon2;
-            $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-            $dist = acos($dist);
-            $dist = rad2deg($dist);
-            $miles = $dist * 60 * 1.1515;
-            $km = $miles * 1.609344;
+            if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+                // Nothing
+            } else {
 
-            $apartament['km'] = $km;
-
-            if ($apartament['km'] <= $radius) {
-                $apps[] = $apartament;
+                $lat2 = $apartament['latitude'];
+                $lon2 = $apartament['longitude'];
+    
+                $theta = $lon1 - $lon2;
+                $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+                $dist = acos($dist);
+                $dist = rad2deg($dist);
+                $miles = $dist * 60 * 1.1515;
+                $km = $miles * 1.609344;
+    
+                $apartament['km'] = $km;
+    
+                if ($apartament['km'] <= $radius) {
+                    $apps[] = $apartament;
+                }
             }
+
         }
 
         return response() -> json(compact('apps'));
