87d6963ad7cb1674cb092179851c14894632bc8e
Marco-S117 <polino47@gmail.com>
2020-03-01 22:57:39 +0100

    Aggiunto controllo lunghezza massima alla descrizione

--- app/Http/Controllers/ApController.php
+++ app/Http/Controllers/ApController.php
@@ -64,7 +64,7 @@ class ApController extends Controller
 
         $data = $request->validate([
             'name' => 'required',
-            'description' => 'required',
+            'description' => 'required|max:255',
             'rooms' => 'required',
             'beds' => 'required',
             'baths' => 'required',
@@ -115,7 +115,7 @@ class ApController extends Controller
 
         $data = $request->validate([
             'name' => 'required',
-            'description' => 'required',
+            'description' => 'required|max:255',
             'rooms' => 'required',
             'beds' => 'required',
             'baths' => 'required',
