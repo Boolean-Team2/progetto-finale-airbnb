903797ef7ad80be71d77308f7b4e26d8ba4a6053
Marco-S117 <polino47@gmail.com>
2020-03-03 19:05:32 +0100

    Aggiunta sezione per il display di eventuale errori messaggi

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,24 @@
 @section('body')
 
 <div class="container my-5 py-5">
+    <div class="row">
+        <div class="col-sm-12 col-md-8 offset-md-2">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul class="m-0">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+    </div>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
